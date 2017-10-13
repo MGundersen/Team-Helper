e887f678a966ef1e0ebe51f4279233a3a977324e
MGundersen <MGundersen95@hotmail.com>
2017-10-13 18:46:02 +0200

    Fixed Spelling Error

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
                 bot.sendMessage({
                   to: userID,
-                  message: 'Hello ' + usedID + ' in private chat!'
+                  message: 'Hello ' + userdID + ' in private chat!'
                 });
             break;
             // !ping
